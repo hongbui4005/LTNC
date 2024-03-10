b4d623b1744a05ea2547c0ffa4742b563d808dba
hongbui4005 <hongbui4005@gmail.com>
2024-03-10 18:15:37 +0700

    LTNC1

---
+++ bai3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    string t;
+
+    while (cin >> t){
+        cout << t << '\n';
+    }
+    return 0;
+}
